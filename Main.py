7db7d7fce81d859022b8361b07305217b384755c
kowsega <110011134+kowsega@users.noreply.github.com>
2022-09-13 11:35:23 +0530

    kowsega

--- Main.py
+++ Main.py
@@ -1,17 +1,21 @@
 from typing import List
 
 def selectionSort(array, size) -> List[int]:
-  for step in range(size):
-    min_idx = step
-    for i in range(step + 1, size):
-      if array[i] < array[min_idx]:
-        min_idx = i
-    (array[step], array[min_idx]) = (array[min_idx], array[step])
-  return array
+    for i in range(size-1):  
+        minIndex = i  
+         
+        for j in range(i+1, size):  
+            if array[j]<array[minIndex]:  
+                minIndex = j  
+                 
+        array[i], array[minIndex] = array[minIndex], array[i]  
+         
+         
+    return array  
 
 # Do not change the following code
 input_data = input()
-data = 
+data = []
 for item in input_data.split(', '):
   if item.isnumeric():
     data.append(int(item))
